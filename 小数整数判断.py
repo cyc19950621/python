4478b4e88842f9e7df75056d1e68db387be523a0
cyc19950621 <1044801968@qq.com>
2018-12-17 15:41:05 +0800

    Update 小数整数判断.py

--- 小数整数判断.py
+++ 小数整数判断.py
@@ -1,32 +1,19 @@
-n=input("输入数据:")
-def check_float(n):
-  #支付时，输入的金额可能是小数，也可能是整数
- s = str(n)
- if s.count('.') == 1: # 判断小数点个数
-     sl = s.split('.') # 按照小数点进行分割
-    left = sl[0] # 小数点前面的
-    right = sl[1] # 小数点后面的
-
-    if left.startswith('-') and left.count('-') == 1 and right.isdigit():
-
-      lleft = left.split('-')[1] # 按照-分割，然后取负号后面的数字
-
-      if lleft.isdigit():
-
-        return False
-
-    elif left.isdigit() and right.isdigit():
-
-      # 判断是否为正小数
-
-      return True
-
-  elif s.isdigit():
-
-    s = int(s)
-
-    if s != 0:
-
-      return True
-
-  return False
+def is_float(str):
+    if str.count('.') == 1: #小数有且仅有一个小数点
+        left = str.split('.')[0]  #小数点左边（整数位，可为正或负）
+        right = str.split('.')[1]  #小数点右边（小数位，一定为正）
+        lright = '' #取整数位的绝对值（排除掉负号）
+        if str.count('-') == 1 and str[0] == '-': #如果整数位为负，则第一个元素一定是负号
+            lright = left.split('-')[1]
+        elif str.count('-') == 0:
+            lright = left
+        else:
+            print('%s 不是小数'%str)
+        if right.isdigit() and lright.isdigit(): #判断整数位的绝对值和小数位是否全部为数字
+            print('%s 是小数'%str)
+        else:
+            print('%s 不是小数'%str)
+    else:
+        print('%s 不是小数'%str)
+n=input(" 输入数据: ")
+print(is_float(n))
